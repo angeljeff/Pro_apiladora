a932b9f7660d8f168805d028ec1f8fd62b6b061b
angeljeff <97898137+angeljeff@users.noreply.github.com>
2023-03-20 09:56:47 -0500

    Update conexion.php

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,19 +1,8 @@
-<?php 
-$host="localhost";
-$db="apiladora";
-$usuario="root";
-$contrasenia="";
-
-try {
-    $conexion= new PDO("mysql:host=$host;dbname=$db",$usuario,$contrasenia);
-    // if ($conexion){
-        
-        
-    //  }
-} catch (Exception $ex) {
-    echo $ex->getMessage();
-    echo "non conectado";
-}
 
 
-?>
+
+
+
+
+
+
